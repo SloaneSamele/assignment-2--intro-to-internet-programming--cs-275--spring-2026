d527d2592cd64f506dacdbb4123325bf5e8a58cc
SloaneSamele <120995022+SloaneSamele@users.noreply.github.com>
2026-03-26 14:25:22 -0400

    Added the script element

--- js/main.js
+++ js/main.js
@@ -1,32 +1,5 @@
-// -----------------------------------------------------------------------------
-// This file includes deliberate formatting errors in order for you to verify
-// that ESLint and EditorConfig are working properly. If both tools are, indeed,
-// working correctly, then you’d see errors in your editor about indentation and
-// improper use of footmarks instead of back ticks. When you save this file,
-// your editor should strip all excess newlines and whitespace characters from
-// the file. If both of these events occur, then ESLint and EditorConfig are
-// working correctly.
-//
-// DON’T PROCEED UNTIL YOU’RE SURE ESLINT AND EDITORCONFIG ARE WORKING CORRECTLY
-// -----------------------------------------------------------------------------
-window.onload = () => {
-       alert('Test');                    
-}
+let script = document.querySelector(`body`);
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+script = document.createElement(`script`);
+script.setAttribute(`src`, `data/data.json`);
+body.appendChild(script);
