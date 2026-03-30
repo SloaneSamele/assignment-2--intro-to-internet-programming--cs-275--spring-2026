379aa5aeaf6b2e676733b8f4ed52b2b71e6407c2
SloaneSamele <120995022+SloaneSamele@users.noreply.github.com>
2026-03-30 07:56:02 -0400

    Added variables with elements

--- js/main.js
+++ js/main.js
@@ -1,5 +1,12 @@
 let script = document.querySelector(`body`);
 let body = document.querySelector(`body`);
+let carousel = document.querySelector(`.carousel-slides`);
+let script = document.createElement(`script`);
+let artist = document.createElement(`h2`);
+let album = document.createElement(`p`);
+let review = document.createElement(`p`);
+let img = document.createElement(`img`);
+let artistURL = document.createElement(`a`);
 
 script = document.createElement(`script`);
 script.setAttribute(`src`, `data/data.json`);
