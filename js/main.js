bc22a40c62176221c9511e72c6a10e3d80373a20
SloaneSamele <120995022+SloaneSamele@users.noreply.github.com>
2026-03-30 12:22:16 -0400

    Added slide variable

--- js/main.js
+++ js/main.js
@@ -1,4 +1,5 @@
 let body = document.querySelector(`body`);
+let slide = 3;
 let carousel = document.querySelector(`.carousel-slides`);
 let script = document.createElement(`script`);
 let artist = document.createElement(`h2`);
@@ -8,13 +9,18 @@ let img = document.createElement(`img`);
 let artistURL = document.createElement(`a`);
 
 function container(data){
-    artist.textContent = data.slides[0].artist;
-    album.textContent = data.slides[0].album;
-    review.textContent = data.slides[0].review.content;
-    img.setAttribute(`src`, data.slides[0].cover_image.path);
-    artistURL.setAttribute(`src`, data.slides[0].url);
-    artistURL.innerText = artist.textContent;
-    console.log(`${data.slides[0].cover_image.path}`);
+    artist.textContent = data.slides[slide].artist;
+    album.textContent = data.slides[slide].album;
+    review.textContent = data.slides[slide].review.content;
+    img.setAttribute(`src`, data.slides[slide].cover_image.path);
+    img.alt = data.slides[slide].cover_image.alt_content;
+    artistURL.setAttribute(`href`, data.slides[slide].url);
+    artistURL.textContent = data.slides[slide].artist;
+    imgCredit.textContent = data.slides[slide].cover_image.credit;
+    imgCredit.href = data.slides[slide].cover_image.url;
+    reviewSource.textContent = 'source: ' + data.slides[slide].review.source;
+    reviewSource.href = data.slides[slide].review.url;
+    console.log(`${img.alt}`);
 }
 script.setAttribute(`src`, `json/data.json`);
 body.appendChild(script);
