bf09cd2324e230c5b10850f8c58dbd9313ab3381
SloaneSamele <120995022+SloaneSamele@users.noreply.github.com>
2026-03-30 07:55:29 -0400

    Added script to body

--- js/main.js
+++ js/main.js
@@ -1,5 +1,7 @@
 let script = document.querySelector(`body`);
+let body = document.querySelector(`body`);
 
 script = document.createElement(`script`);
 script.setAttribute(`src`, `data/data.json`);
+script.setAttribute(`src`, `json/data.json`);
 body.appendChild(script);
