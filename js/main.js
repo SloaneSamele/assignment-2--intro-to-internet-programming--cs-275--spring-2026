9bf4eaf7bf99a5b3c918cd5f7b7a29474d546c7b
SloaneSamele <120995022+SloaneSamele@users.noreply.github.com>
2026-04-01 23:35:59 -0400

    Removed artist h2 and fixed review to the left

--- js/main.js
+++ js/main.js
@@ -6,6 +6,7 @@ let script = document.createElement(`script`);
 let leftArrow = document.querySelectorAll(`a`)[0];
 let rightArrow = document.querySelectorAll(`a`)[1];
 leftArrow.style.visibility = `hidden`;
+
 function container(data){
     for(let index = 0; index < data.slides.length; ++index){
         let artist = document.createElement(`h2`);
@@ -18,7 +19,7 @@ function container(data){
         let slide = document.createElement(`div`);
 
         slide.classList.add(`carousel-slide`);
-        artist.textContent = data.slides[index].artist;
+
         album.textContent = data.slides[index].album;
         artistURL.href = data.slides[index].url;
         artistURL.textContent = data.slides[index].artist;
@@ -29,11 +30,13 @@ function container(data){
         imageCredit.textContent = data.slides[index].cover_image.credit;
 
         review.textContent = data.slides[index].review.content;
+        review.style.textAlign = `start`;
         reviewCredit.textContent = data.slides[index].review.source;
         reviewCredit.href = data.slides[index].review.url;
 
         slide.appendChild(artist);
         slide.appendChild(album);
+        slide.appendChild(artistURL);
         slide.appendChild(coverImage);
         slide.appendChild(imageCredit);
         slide.appendChild(review);
